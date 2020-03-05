9d8a2ec5f997b1c82d4e5506696603fa772f4d21
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-05 14:55:17 +1300

    Update Sales Processing Delays.sql

--- Sales Processing Delays.sql
+++ Sales Processing Delays.sql
@@ -1,14 +1,12 @@
 -- Lists all sales in the database since a specific date and time that have a processing delay of x hours.
 
-DECLARE @SaleDate date
-DECLARE @Age int
-DECLARE @AgeMeasure varchar(2)
+DECLARE @DateFrom date 
+DECLARE @ProcessingDelay int
 
 -- Change these as required
-SET @SaleDate = DATEADD(day,-1,GETDATE())
-SET @Age = 3
-SET @AgeMeasure = hh -- hours
+SET @DateFrom = DATEADD(day,-7,GETDATE())  --Now minus 1 day
+SET @ProcessingDelay = 3 -- Took longer than 3 hours
 
 SELECT TransactionID, SaleDate, ProcessedDate, tl.LocationDescription, TerminalNo, DATEDIFF(hh, SaleDate, ProcessedDate) AS ProcessingDelay, SaleTotal from terminalsales 
   join TerminalLocationTable tl on tl.LocationNo = terminalsales.LocationNo
-  where SaleDate > @SaleDate and SaleType in (0,8) and DATEDIFF(@AgeMeasure, SaleDate, ProcessedDate) > @Age order by SaleDate DESC
+  where SaleDate > @DateFrom and SaleType in (0,8) and DATEDIFF(hh, SaleDate, ProcessedDate) > @ProcessingDelay order by SaleDate DESC
