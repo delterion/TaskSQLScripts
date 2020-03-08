98a4fbec3dda7bcccbd851bb257714596308c396
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-09 11:42:35 +1300

    Script to update terminal update times

---
+++ Terminal Update Time.sql
@@ -0,0 +1,12 @@
+﻿-- Change the Update Check Time for the Terminals.
+USE Task_PROD
+
+DECLARE @UpdateTime float
+
+-- Change these as required
+SET @UpdateTime = 60
+
+UPDATE TerminalSetupTable
+  SET CUI = @UpdateTime
+WHERE CUI = 1440
+
