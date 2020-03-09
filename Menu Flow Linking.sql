2070fc996dc4c448dd17717d4647512d2d0de3e6
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-10 10:45:18 +1300

    Create Menu Flow Linking.sql

---
+++ Menu Flow Linking.sql
@@ -0,0 +1,8 @@
+﻿-- ## List out the menu flow windows
+select * from TerminalMenuFlowDataOnly where MenuFlowDataId = 763
+
+-- ## Find the Family Settings within the Menu Flow
+select * from TerminalMenuFlowDataOnlyFamilySettings where TMFDataId = 763
+
+-- ## List the settings for the part of the menu flow.
+select * from TerminalMenuFlowMenuSettings where id = 2309
