43fd5f7ef44493cefef0a94b0d80a60ce494c628
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-10 08:40:49 +1300

    Update Software Version Check.sql

--- Software Version Check.sql
+++ Software Version Check.sql
@@ -1,3 +1,14 @@
 -- ## Check Software Version on Terminals assigned to a Location
 select TerminalNo, VenueNo, SoftwareVersion from TerminalSetupTable
   where TerminalNo in (select terminalno from terminalsetuptable where LocationNo = 79)
+
+-- ## Count of Terminals with Version
+select SoftwareVersion, count(*) as Count from TerminalSetupTable
+  group by SoftwareVersion
+
+-- ## Count of Terminals with Version by Outlet
+select TerminalLocationTable.LocationDescription, SoftwareVersion, count(*) as Count from TerminalSetupTable JOIN TerminalLocationTable on TerminalSetupTable.LocationNo = TerminalLocationTable.LocationNo
+  where SoftwareVersion like '19.1.2%'
+  group by TerminalLocationTable.LocationDescription, SoftwareVersion
+  Order by TerminalLocationTable.LocationDescription
+  
