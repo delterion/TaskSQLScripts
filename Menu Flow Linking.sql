3c9526217ee7341ba50586640845c1d875634f46
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-10 15:19:28 +1300

    Update Menu Flow Linking.sql

--- Menu Flow Linking.sql
+++ Menu Flow Linking.sql
@@ -1,8 +1,20 @@
-﻿-- ## List out the menu flow windows
-select * from TerminalMenuFlowDataOnly where MenuFlowDataId = 763
+﻿DECLARE @MenuFlowID int
+DECLARE @MenuFlowDataID int
+DECLARE @MenuFlowSettingsID int
+
+SET @MenuFlowID  = 1132
+SET @MenuFlowDataID = (SELECT ID from TerminalMenuFlowData where MenuFlowID = @MenuFlowID)
+SET @MenuFlowSettingsID = (SELECT MenuFlowSettingsId from TerminalMenuFlowData where MenuFlowID = @MenuFlowID)
+
+SELECT * FROM TerminalMenuFlow where MenuFlowID = @MenuFlowID
+
+SELECT * from TerminalMenuFlowData where MenuFlowID = @MenuFlowID
+
+-- ## List out the menu flow windows
+select * from TerminalMenuFlowDataOnly where MenuFlowDataId = @MenuFlowDataID
 
 -- ## Find the Family Settings within the Menu Flow
-select * from TerminalMenuFlowDataOnlyFamilySettings where TMFDataId = 763
+select * from TerminalMenuFlowDataOnlyFamilySettings where TMFDataId = @MenuFlowDataID
 
 -- ## List the settings for the part of the menu flow.
-select * from TerminalMenuFlowMenuSettings where id = 2309
+select * from TerminalMenuFlowMenuSettings where id = @MenuFlowSettingsID
