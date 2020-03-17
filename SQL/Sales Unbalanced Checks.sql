3919071245a5b427c7ea137274c3419b64612a2e
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-18 09:04:59 +1300

    Update Sales Unbalanced Checks.sql

--- SQL/Sales Unbalanced Checks.sql
+++ SQL/Sales Unbalanced Checks.sql
@@ -31,7 +31,7 @@ set @tableuuid = NULL
 set @parentuuid = NULL
 set @mergedtransaction = NULL
 
-CREATE Table #mytransaction
+CREATE Table #mytransaction 
    (transactionid int, date datetime,
     total money,
     itemtotal money,
@@ -53,8 +53,8 @@ set ROWCOUNT 0
 --select TransactionID into #mytemp from TerminalSales where saletype=0 and TransactionID in (2367438,2216112,2330022,2335646,2262963,2253938,2228717,2228710,2168720,2125747,2115542,2356760,2341285,2259019,2192845,2334040,2308775,2262094,2239512,2206905,2335473,2317935,2247431,2350911,2295837,2278962,2278940,2305570,2126094,2246341,2300222,2372440,2359182,2257160,2098834,2338229,2193665,2318321,2083086,2241727,2272463,2369581,2102192,2327083,2315881,2278613,2242909,2357535,2243627,2332097,2247439,2286695,2212209,2300801,2216393,2065005,2286697,2346888,2247444,2139795,2313858,2258397,2131543,2354867,2303186,2354880,2262514,2354876,2313996,2357323,2082064,2357722,2246365,2356388,2262352,2357051,2121034,2340853,2338130,2336650,2331461,2320703,2304656,2285324,2283457,2250658,2236155,2229151,2227530,2219340,2217395,2201710,2134166,2130105,2346966,2344932,2344692,2360582,2285983,2134494,2246941,2196864,2131924,2370875,2346804,2324074,2258294,2288509,2106594,2326797,2306863,2101263,2275301,2233005,2360382,2371775,2254082,2284794,2321119,2315496,2228467,2344907,2360005,2218755,2291163,2264488,2372171)
 --select transactionID into #mytemp from TerminalSales where TransactionID in (2034692,2043710,2048737,2061223,2070445,2088951,2090258,2090259,2091471,2091472,2092173,2092174,2098310,2133940,2144195,2160181,2162852,2162855,2162942,2167747,2171333,2172009,2173789,2177224,2178079,2184936,2186986,2191322,2191851,2192295,2192296,2193508,2193531,2201704,2205653,2208658,2226789,2229370,2233043,2233044,2234247,2235510,2236770,2238911,2238920,2241235,2243627,2245787,2247431,2247439,2247444,2249027,2254601,2257859,2258158,2259630,2259881,2263160,2265574,2274378,2274424,2275335,2277756,2280303,2281689,2283810,2284634,2287625,2291972,2291973,2292376,2292906,2292989,2294646,2295129,2295733,2295956,2296005,2298316)
 --select TransactionID into #mytemp from TerminalSales where TransactionID >= 2033463
---select TransactionID into #mytemp from TerminalSales where SaleDate > '2020-01-01 00:00:00' and SaleType in (0,8)
-select TransactionID into #mytemp from TerminalSales where SaleDate > DATEADD(day,-7,GETDATE()) and SaleType in (0,8)
+select TransactionID into #mytemp from TerminalSales where SaleDate > '2020-03-01 00:00:00' and SaleType in (0,8)
+--select TransactionID into #mytemp from TerminalSales where SaleDate > DATEADD(day,-7,GETDATE()) and SaleType in (0,8)
 
 set ROWCOUNT 1
 select @transactionid = transactionid from #mytemp
@@ -72,16 +72,15 @@ while @@rowcount <> 0
 	--IF @diff = 0 GOTO Next_transaction --GOTO Next Transaction
 
   	select @tablenumber = tablenumber from TerminalSales where TransactionID = @transactionid
-  	--select @member = MemberNo from terminalsales where TransactionID = @transactionid
     select @tableuuid = TableChargeHistoryTableGUID from TerminalSales where TransactionID = @transactionid
     select @parentuuid = PreSplitTableGUID from TerminalTableChargeHistoryTableStart where TableChargeHistoryTableGUID = @tableuuid
     select @mergedtransaction = FinalisingTransactionID from TerminalTableChargeHistoryTableStart where PreSplitTableGUID = @parentuuid
-	select @clerkid = ClerkID from TerminalSales where TransactionID = @transactionid
-	SELECT @terminalid = TerminalNo from TerminalSales where TransactionID = @transactionid
-	select @locationid = locationno from TerminalSales where transactionid = @transactionid
-	select @location = LocationDescription from TerminalLocationTable where LocationNo = @locationid
-	select @rounding = sales from terminalsalesitems where transactionid = @transactionid and productid = -99999
-	select @date = saledate from terminalsales where transactionid = @transactionid
+	  select @clerkid = ClerkID from TerminalSales where TransactionID = @transactionid
+  	select @terminalid = TerminalNo from TerminalSales where TransactionID = @transactionid
+  	select @locationid = locationno from TerminalSales where transactionid = @transactionid
+  	select @location = LocationDescription from TerminalLocationTable where LocationNo = @locationid
+  	select @rounding = sales from terminalsalesitems where transactionid = @transactionid and productid = -99999
+  	select @date = saledate from terminalsales where transactionid = @transactionid
 
 	if @tablenumber <> 0 
     BEGIN
@@ -130,7 +129,7 @@ while @@rowcount <> 0
 	Select @quantity = SUM(qty) from terminalsalesitems where TransactionID = @transactionid
 
 	
-	SET @filter = 0
+	SET @filter = 1
 
 	IF @filter = 1
 	BEGIN
@@ -182,13 +181,13 @@ while @@rowcount <> 0
     set @tableuuid = NULL
     set @parentuuid = NULL
     set @mergedtransaction = NULL
-	set @rounding = NULL
-	set @date = NULL
-	set @quantity = NULL
-	set @tip = NULL
-
+	  set @rounding = NULL
+  	set @date = NULL
+  	set @quantity = NULL
+  	set @tip = NULL
+    
   	delete #mytemp where TransactionID = @transactionid
-	
+	  
     set ROWCOUNT 1
     select @transactionid = transactionid from #mytemp
   END
