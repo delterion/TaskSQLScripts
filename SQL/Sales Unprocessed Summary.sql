5d04b0f8251666f50146116c255768e7b7281bd5
Jonathan Prentice <jonathan@prevas.co.nz>
2020-03-23 10:02:49 +1300

    Couple of Updates

---
+++ SQL/Sales Unprocessed Summary.sql
@@ -0,0 +1,8 @@
+﻿-- This will output for you if there are any sales errors in the DB
+USE Task_PROD
+
+select TerminalLocationTable.LocationDescription, TerminalSalesProcessTable.TerminalNo, COUNT(*) as Checks, SUM(TerminalSales.SaleTotal) as Amount from TerminalSalesProcessTable
+  join TerminalSales on TerminalSalesProcessTable.TerminalNo = TerminalSales.TerminalNo
+  join TerminalLocationTable on TerminalSales.LocationNo = TerminalLocationTable.LocationNo
+where HasError = 1
+GROUP BY TerminalLocationTable.LocationDescription, TerminalSalesProcessTable.TerminalNo
