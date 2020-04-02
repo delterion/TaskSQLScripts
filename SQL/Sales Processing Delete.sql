351c4d68f24f5d1b39c18eccb8fc95a57cbf39c2
Jonathan Prentice <jonathan@prevas.co.nz>
2020-04-03 08:25:37 +1300

    Few script updates

---
+++ SQL/Sales Processing Delete.sql
@@ -0,0 +1,4 @@
+﻿## This script deletes all sales in the sales processing table where the termional number equals the setting below.
+
+delete from TerminalSalesProcessTable
+  where TerminalNo = 28321
--- SQL/Sales Unprocessed Summary.sql
+++ SQL/Sales Unprocessed Summary.sql
@@ -4,5 +4,7 @@ USE Task_PROD
 select TerminalLocationTable.LocationDescription, TerminalSalesProcessTable.TerminalNo, COUNT(*) as 'Check#', SUM(TerminalSales.SaleTotal) as Amount from TerminalSalesProcessTable
   join TerminalSales on TerminalSalesProcessTable.TerminalNo = TerminalSales.TerminalNo
   join TerminalLocationTable on TerminalSales.LocationNo = TerminalLocationTable.LocationNo
-where HasError = 1
+--where HasError = 1
 GROUP BY TerminalLocationTable.LocationDescription, TerminalSalesProcessTable.TerminalNo
+
+
