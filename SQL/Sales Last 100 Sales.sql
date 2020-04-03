826a1cce3a762e4daedcb49e87929cfbadbcff09
Jonathan Prentice <jonathan@prevas.co.nz>
2020-04-03 14:25:44 +1300

    Update Sales Last 100 Sales.sql

--- SQL/Sales Last 100 Sales.sql
+++ SQL/Sales Last 100 Sales.sql
@@ -3,3 +3,4 @@
 select top 100 TerminalLocationTable.LocationDescription, TerminalSales.TerminalNo, TerminalSales.TransactionID, TerminalSales.SaleTotal as Amount, SaleDate from TerminalSales
   join TerminalLocationTable on TerminalSales.LocationNo = TerminalLocationTable.LocationNo
 where SaleType in (0,8) order BY SaleDate DESC
+
