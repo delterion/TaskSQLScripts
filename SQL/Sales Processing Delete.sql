3ef9c46b9c00304650be9a3f98eeb06ba93cd323
Jonathan Prentice <jonathan@prevas.co.nz>
2020-04-08 07:59:37 +1200

    small updates

--- SQL/Sales Processing Delete.sql
+++ SQL/Sales Processing Delete.sql
@@ -1,4 +1,6 @@
 ﻿-- This script deletes all sales in the sales processing table where the termional number equals the setting below.
 
+-- delete from TerminalSalesProcessTable where TerminalNo = 28321
+
 delete from TerminalSalesProcessTable
-  where TerminalNo = 28321
+  where TerminalNo in(28321, 28322, 28381)
