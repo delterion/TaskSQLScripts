a9e159c850bd12747a28866dcbc0a5c4fb920694
Jonathan Prentice <jonathan@prevas.co.nz>
2020-07-15 11:43:06 +1200

    Minor Updates

---
+++ SQL/Clerk Duplicate Finder.sql
@@ -0,0 +1,12 @@
+﻿
+SELECT a.ClerkID, a.ClerkName, a.ClerkSurname, a.ClerkLogon, a.PayrollIDNumber, a.IsActive, a.SalesTotal
+FROM TerminalClerkTable a
+JOIN (SELECT PayrollIDNumber, COUNT(*) as Count
+FROM TerminalClerkTable
+WHERE IsActive = 1
+GROUP BY PayrollIDNumber
+HAVING count(*) > 1 ) b
+ON a.PayrollIDNumber = b.PayrollIDNumber
+ORDER BY a.PayrollIDNumber
+
+
