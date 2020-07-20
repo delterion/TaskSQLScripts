2462efef93f4707b368a077c23fccc3b3e6a780e
Jonathan Prentice <jonathan@prevas.co.nz>
2020-07-20 13:12:04 +1200

    Update Sales Processing Errors.sql

--- SQL/Sales Processing Errors.sql
+++ SQL/Sales Processing Errors.sql
@@ -4,3 +4,7 @@ USE Task_PROD
 select * from TerminalSalesProcessTable
 where HasError = 1
 
+select tsp.terminalno, tlt.LocationDescription, qty = (select count(tsp1.terminalno) from terminalsalesprocesstable tsp1 where tsp1.TerminalNo = tsp.terminalno) from TerminalSalesProcessTable tsp
+  join TerminalSetupTable tst on tsp.TerminalNo = tst.TerminalNo
+  join TerminalLocationTable tlt on tst.LocationNo = tlt.LocationNo 
+where tsp.HasError = 1
